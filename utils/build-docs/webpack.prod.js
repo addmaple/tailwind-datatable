ad4cd35cfa8ab041fa29b4874507d008c7794fe5
Hitra Dostava <hitradostava@addmaple.com>
2021-04-09 05:08:49 +0200

    Change public path

--- utils/build-docs/webpack.prod.js
+++ utils/build-docs/webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require("path")
 
 module.exports = {
+  output: {
+    publicPath: "/open-source/tailwind-datatable",
+  },
   mode: "production",
   devServer: {
     contentBase: path.resolve(__dirname, "../../public"),
